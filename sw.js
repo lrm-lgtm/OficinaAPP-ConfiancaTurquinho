90be85c1748c728d5aaa59dd75cec02c520c5518
lrm-lgtm <leonardo.onipres@gmail.com>
2026-09-21 21:40:10 -0300

    chore: refresh PWA shell for full UI rebuild

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="oficina-demo-v3";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
+const CACHE="oficina-demo-v4";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>{
