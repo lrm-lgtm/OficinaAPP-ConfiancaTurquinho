af9d649edb34b47c761557d7014dd9509d35ded5
lrm-lgtm <leonardo.onipres@gmail.com>
2026-09-22 04:30:57 -0300

    chore: bump PWA cache to v11.1

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="oficina-v11-0-1";
+const CACHE="oficina-v11-1";
 const STATIC_ASSETS=[
-  "./styles.css?v=11.0.1",
-  "./app.js?v=11.0.1",
+  "./styles.css?v=11.1",
+  "./app.js?v=11.1",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
